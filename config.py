6b4c1cae55bf326a075f421283d0ef63cc841917
Vignesh pandian A <masterpiecevpa@gmail.com>
2026-03-03 11:49:40 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+LLAMA_MODEL_PATH = "models/llama.gguf"
+VOSK_MODEL_PATH = "models/vosk-model"
+
+SAMPLE_RATE = 16000
+BLOCK_SIZE = 8000
+
+TTS_SAMPLE_RATE = 48000
+SPEAKER = "hindi_female"
